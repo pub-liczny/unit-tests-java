fb0e5012fc725927dc068712a6853be2879b7992
Beger Mikołaj <mikolaj.beger@atena.pl>
2023-02-24 08:30:47 +0100

    3.9 Disabled and DisplayName

--- src/test/java/pl/devfoundry/testing/CartTest.java
+++ src/test/java/pl/devfoundry/testing/CartTest.java
@@ -1,13 +1,20 @@
 package pl.devfoundry.testing;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@Disabled
+@DisplayName("Test cases for Cart")
 class CartTest {
 
+//    @Ignore   // Dla JUunit4
+//    @Disabled
+    @DisplayName("Cart is able to process 1000 orders in 100 ms")
     @Test   // Dla JUnit4 dodawłąo się do adnotacji (timeout = 100)
     void simulateLargeOrder() {
         //given
